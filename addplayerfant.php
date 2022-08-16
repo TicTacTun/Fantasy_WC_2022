ff332134adeb219fed9c722716be75d5d3cddce2
Md Shakibul Alam <70951378+Alam2002@users.noreply.github.com>
2022-08-16 22:41:47 +0600

    update

---
+++ addplayerfant.php
@@ -0,0 +1,18 @@
+<?php
+require_once('datbasecon.php');
+
+if (isset($_GET['addid'])){
+    $id = $_GET['addid'];
+    
+    
+
+    $sql = "INSERT INTO abraca (Name, Position) SELECT Name, Position FROM players where Player_ID =$id; ";
+    $result = mysqli_query($conn,$sql);
+    if ($result ){
+        header('location:team_create1.php');
+    }
+    else{
+        mysqli_error($conn);
+    }
+}
+?>
