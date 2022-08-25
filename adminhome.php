3f5efc971279f924069121cc57d1ce4339faa8d6
TicTacTun <kadir.hasan@g.bracu.ac.bd>
2022-08-25 21:07:02 +0600

    updated adminhome

--- adminhome.php
+++ adminhome.php
@@ -1,3 +1,33 @@
 <?php
-echo "This is admin home"; 
+  require_once 'config.php';
+  session_start();
+  $uname = $_SESSION['username'];
+  $email = $_SESSION['email-log'];
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Fantasy WC 2022 Admin Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="styleadminhome.css">
+</head>
+<body>
+    <header>
+        <div class="main">    
+            <ul>
+                <li class="active"><a href="#">Admin Home</a></li>
+                <li><a href="userhandle.php">Users</a></li>
+                <li><a href="#">Fantasy Teams</a></li>
+                <li><a href="#">Players</a></li>
+                <li><a href="#">Points</a></li>
+            </ul>
+        </div>
+        <div class="title">
+            <h1>Admin Dashboard</h1>
+        </div>
+        <div class="button">    
+            <a href="logout.php" class="btn">Logout</a>
+        </div>
+    </header>
+</body>
+</html>
