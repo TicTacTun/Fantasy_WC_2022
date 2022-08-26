366c792403610477c40480dde8b56ae8079afd82
TicTacTun <kadir.hasan@g.bracu.ac.bd>
2022-08-26 20:49:00 +0600

    name chnaged

--- fantasyadmin.php
+++ fantasyadmin.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="fanadmin.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     
-    <title>Document</title>
+    <title>Fantasy Team Manage</title>
 </head>
 <body>
     <header>
