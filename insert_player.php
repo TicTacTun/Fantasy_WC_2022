faef746ea7014355c6c9ab5e6d518e6cf0130a7b
TicTacTun <kadir.hasan@g.bracu.ac.bd>
2022-08-27 21:50:03 +0600

    css changed

--- insert_player.php
+++ insert_player.php
@@ -1,6 +1,7 @@
 <?php
 // Include config file
 require_once "config.php";
+
  
 // Define variables and initialize with empty values
 $country = $id = $position = $name = $age = $apps = $goals = "";
@@ -133,7 +134,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Insert Player</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="matches.css">
-    <link rel="stylesheet" type="text/css" href="styleadminhome.css">
     <style>
         .wrapper{
             width: 600px;
